b5df03e0eb642dfe8a6ac1f298270c3498fe6e67
Aditya Kajla <akajla09@users.noreply.github.com>
2023-01-04 16:58:02 -0800

    Add goreleaser + other ci jobs (#16)
    * Add goreleaser + other ci jobs
    
    * Fix goreleaser deprecated config

--- cmd/apirunner/main.go
+++ cmd/apirunner/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/warrant-dev/apirunner"
 )
@@ -11,7 +12,7 @@ const RunConfigFilename = "apirunner.conf"
 
 func main() {
 	dirArg := os.Args[1]
-	passed, err := apirunner.Run(RunConfigFilename, dirArg)
+	passed, err := apirunner.Run(filepath.Join(dirArg, RunConfigFilename), dirArg)
 	if err != nil {
 		fmt.Printf("Error executing tests: %v\n", err)
 		os.Exit(1)
